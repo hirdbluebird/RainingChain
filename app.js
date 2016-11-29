229cfe42de3d8819659ce1dc91dc2b4b51a0ac04
hirdbluebird <bluebirdyourdestiny@gmail.com>
2016-11-29 19:32:01 +0200

    Handling multiple websocket connections

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
-var io = require('socket.io')(serv,{});
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
@@ -11,14 +10,36 @@ app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
 console.log('server started');
 
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-	console.log('socket connection');
+	socket.id = Math.random();
+	socket.x = 0;
+	socket.y = 0;
+	socket.number = '' + Math.floor(10 * Math.random());
+	SOCKET_LIST[socket.id] = socket;
 
-	socket.on('happy',function(data){
-		console.log('happy because ' + data.reason);
-	});
-
-	socket.emit('serverMsg', {
-		msg:'hello',
+	socket.on('disconnect',function(){
+		delete SOCKET_LIST[socket.id];
 	});
 });
+
+setInterval(function(){
+	var pack = [];
+
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			number:socket.number
+		});
+	}
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack);
+	}
+},1000/25)
